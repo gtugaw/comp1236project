8884aa01f5c701fd81c46a30cc200fad263b6766
gtugaw <tugawinga@gmail.com>
2025-04-07 03:47:53 +0000

    add code for entering values and some initial logic

--- code/task3.sh
+++ code/task3.sh
@@ -1,8 +1,34 @@
 # This is where task 3 code must be place
 
-var1=80
-var2=2
-var3=$[$var1 / $var2]
-echo The final result is $var3
+echo "For the quadratic sequence term = an^2 + bn + c where n ranges fron n1 to n2, please input the values."
 
-echo "Please copy me!"
+read -p "Enter the value of (a): " a
+read -p "Enter the value of (b): " b
+read -p "Enter the value of (c): " c
+read -p "Enter the value of (n1): " n1
+read -p "Enter the value of (n2): " n2
+
+sum=0
+count=0
+n=n1
+product=1
+
+term=$[a*n*n+b*n+c]
+
+echo "The value of term where n=$[n] is $[term]."
+
+if [ $[n] == $[n1] ]
+then
+    product=term
+fi
+
+sum=$[sum+term]
+
+if [ $[n] == $[n2] ]
+then
+    echo "The count of prime is $[count]."
+else
+    n=$[n+1]
+fi
+
+
