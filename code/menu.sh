42a9e5c535a299d430bfdbdba5ae1f8cf0b478d2
gtugaw <tugawinga@gmail.com>
2025-04-08 04:26:29 +0000

    added password checking in the menu

--- code/menu.sh
+++ code/menu.sh
@@ -1,1 +1,25 @@
+#!/bin/bash
 # Bash code for the Menu
+
+password=""
+warning=0
+
+while [[ $password != "w22" ]]
+do
+    if [ $warning -eq 3 ]
+    then
+        exit
+    fi
+
+    if [ $warning -eq 2 ]
+    then
+        echo "Warning: Only one left attempt to input the correct password!"
+    fi
+
+    read -p "Please enter the password: " password
+    #echo "$password"
+
+    warning=$[ warning+1 ]
+done
+
+echo "The password is correct."
