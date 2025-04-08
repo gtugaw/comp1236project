944d42602fe7d2a942ab904ba9a4f6ddcbaad2d8
gtugaw <tugawinga@gmail.com>
2025-04-08 01:30:46 +0000

    added prime number checker

--- code/task3.sh
+++ code/task3.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+
 # This is where task 3 code must be place
 
 echo "For the quadratic sequence term = an^2 + bn + c where n ranges fron n1 to n2, please input the values."
@@ -9,18 +11,50 @@ read -p "Enter the value of (n1): " n1
 read -p "Enter the value of (n2): " n2
 
 sum=0
-count=$[n2-n1+1]
-n=n1
+# count=$[ n2-n1+1 ]
+count=0
+n=$[ n1 ]
 product=1
 
-term=$[a*n*n+b*n+c]
+while [ $n -le $n2 ]
+do
+    term=$[ a*n*n+b*n+c ]
+    sum=$[ sum+term ]
+
+    echo "$[n] $[n2] $[term] $[sum]"
+
+    if [ $n -eq $n1 ]
+    then
+        product=term
+        echo "$[product]"
+    fi
+    
+    modulo=2
+
+    while [ $[term%modulo] -ne 0 ]
+    do
+        modulo=$[ modulo+1 ]
+        echo "$[term] $[modulo]"
+
+        if [ $modulo -eq $term ]
+        then
+            count=$[ count+1 ]
+            echo "$[term] is a prime number."
+        fi
+    done
+
+    echo "$[count]"
+ 
+    n=$[ n+1 ]
+done
+
+
+
+
 
 echo "The value of term where n=$[n] is $[term]."
 
-if [ $[n] == $[n1] ]
-then
-    product=term
-fi
+
 
 sum=$[sum+term]
 
@@ -31,5 +65,6 @@ then
 else
     n=$[n+1]
 fi
+
 
 
