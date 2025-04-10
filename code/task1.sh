4a6f8855ddabc3227cf9c338bba31562b841228e
gtugaw <tugawinga@gmail.com>
2025-04-10 18:47:12 +0000

    formatted text for readability and converted script to function

--- code/task1.sh
+++ code/task1.sh
@@ -1,54 +1,60 @@
 # Finding Hexagonal Numbers In Range
 # Segregate The Odd And Even Numbers
 # Find The Sum Of Odd And Even Numbers
+#
+# Coded by
+# Student: Disha Padsala
+# ID: 101581979
+#
 
-read -p "Enter the lower bound a: " a
-read -p "Enter the upper bound b: " b
+function task1 {
 
-n=1
-odd_count=0
-even_count=0
-odd_sum=0
-even_sum=0
-hexagonal_numbers=()
+    read -p "Enter the lower bound a: " a
+    read -p "Enter the upper bound b: " b
 
-echo "hexagonal numbers in range [$a, $b]"
+    n=1
+    odd_count=0
+    even_count=0
+    odd_sum=0
+    even_sum=0
+    hexagonal_numbers=()
 
-while true
-do
+    echo "hexagonal numbers in range [$a, $b]"
 
-hex=$((n * (2 * n -1)))
+    while true
+    do
 
-
-if [ $hex -ge $b ]
-then
-break
-
-fi
-
-if [ $hex -ge $a ]
-then
-        hexagonal_numbers+=($hex)
-
-if (( hex % 2 == 0 ))
-then
-        echo "The number is even: $hex."
-        (( even_count++ ))
-        (( even_sum += hex ))
-else
-        echo "The number is odd: $hex."
-        (( odd_count++ ))
-        (( odd_sum += hex ))
-fi
-fi
-((n++))
-done
-
-#output
-
-echo "even count: $even_count"
-echo "odd count: $odd_count"
-echo "sum of even hexagonal numbers: $even_sum"
-echo "sum of odd hexagonal numbers: $odd_sum"
+        hex=$((n * (2 * n -1)))
 
 
+        if [ $hex -ge $b ]
+        then
+            break
+
+        fi
+
+        if [ $hex -ge $a ]
+        then
+            hexagonal_numbers+=($hex)
+
+            if (( hex % 2 == 0 ))
+            then
+                echo "The number is even: $hex."
+                (( even_count++ ))
+                (( even_sum += hex ))
+            else
+                echo "The number is odd: $hex."
+                (( odd_count++ ))
+                (( odd_sum += hex ))
+            fi
+        fi
+        ((n++))
+    done
+
+    #output
+
+    echo "even count: $even_count"
+    echo "odd count: $odd_count"
+    echo "sum of even hexagonal numbers: $even_sum"
+    echo "sum of odd hexagonal numbers: $odd_sum"
+}
