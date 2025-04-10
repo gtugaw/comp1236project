46388168e52c67cf48a810127dd45c248820404a
gtugaw <tugawinga@gmail.com>
2025-04-10 18:57:50 +0000

    formatted text

--- code/task2.sh
+++ code/task2.sh
@@ -1,10 +1,14 @@
-    #Task 2 Working with Square Numbers
-    #Find square numbers in odd positions, starting from a user input initial position and a range.
-    #From those square numbers, find those that are factors of x, a user input value.
-    #Sum consecutive square numbers until a maximum value entered by user.
-    #Code by Maria Tai
+# Task 2 Working with Square Numbers
+# Find square numbers in odd positions, starting from a user input initial position and a range.
+# From those square numbers, find those that are factors of x, a user input value.
+# Sum consecutive square numbers until a maximum value entered by user.
+# Code by
+# Student: Maria Tai
+# ID: 101563558
+#
 
 function task2 {
+
     echo "Task 2: Working with Square Numbers "
     read -p "From what position do you want to start?: " initialPosition
     read -p "How many square numbers do you want to see?: " range
@@ -22,18 +26,18 @@ function task2 {
 
     while [ $squareCount -le $range ]
     do
-            squareNum=$((initialPosition**2))
-            oddNum=$((squareNum%2))
-            factor=$((x%squareNum))
-            if [ $oddNum -eq 1 ]
+        squareNum=$((initialPosition**2))
+        oddNum=$((squareNum%2))
+        factor=$((x%squareNum))
+        if [ $oddNum -eq 1 ]
             then
-                    if [ $factor -eq 0 ]
-                    then
-                            echo "$squareNum --> is a factor of $x. "
-                    else
-                            echo $squareNum
-                    fi
-                    squareCount=$((squareCount+1))
+                if [ $factor -eq 0 ]
+                then
+                    echo "$squareNum --> is a factor of $x. "
+                else
+                    echo $squareNum
+                fi
+                squareCount=$((squareCount+1))
             fi
             initialPosition=$((initialPosition + 1))
     done
@@ -45,17 +49,17 @@ function task2 {
 
     while [ $squareSum -lt $maxSum ]
     do
-            firstSquare=$((num**2))
-            consNum=$((num+1))
-            consSquare=$((consNum**2))
-            squareSum=$((firstSquare+consSquare))
-            if [ $squareSum -lt $maxSum ]
-            then
-                    echo $squareSum
-                    count=$((count+1))
+        firstSquare=$((num**2))
+        consNum=$((num+1))
+        consSquare=$((consNum**2))
+        squareSum=$((firstSquare+consSquare))
+        if [ $squareSum -lt $maxSum ]
+        then
+            echo $squareSum
+            count=$((count+1))
 
-            fi
-            num=$((num+1))
+        fi
+        num=$((num+1))
     done
     echo "Count of the sum of the consecutive square numbers: $count. "
 }
